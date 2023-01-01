012b815f4a9afeffd6d350fc8c6eaa47aeca7ef4
Tomasz Madej <57150210+kryptoTomo@users.noreply.github.com>
2023-01-01 17:37:13 +0100

    Update axios.js

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://flask-app-filmmap.onrender.com/'
+    baseURL: 'https://movie-platform-backend.onrender.com/'
 });
 
 export default instance;
